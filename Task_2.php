868740e5844e24629ede6bca55c7dcb81fe67b74
Kathirvelprakash <kathirvelprakash.m@gmail.com>
2025-03-15 17:47:59 +0530

    Task_2.php is Uploaded

---
+++ Task_2.php
@@ -0,0 +1,29 @@
+<?php
+
+$name = "Kathir";
+$age = 24;
+$height = 6.2;
+$is_student = true;
+
+echo "Original Height: $height <br>";
+
+$height = (int) $height;
+echo "Casted Height: $height <br><br>";
+
+$a = 5;
+
+$b = $a + 3;
+echo "Addition Output: $b <br>";
+
+$b = $a * 3;
+echo "Multiplication Output: $b <br>";
+
+$b = $a / 3;
+echo "Division Output:". number_format($b,2)."<br>";
+
+$b = $a - 3;
+echo "Subtraction Output: $b <br>";
+
+$b = $a % 3;
+echo "Modulus Output: $b <br>";
+?>
